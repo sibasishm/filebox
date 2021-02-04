1553f18a32aa7de8fd1138bdb0e4e94c604096c2
Sibasish Mohanty <smsibasish@gmail.com>
2021-02-04 15:36:36 +0530

    dependency cleanup

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ const App = () => (
 		<div
 			css={[
 				tw`flex flex-col items-center justify-center h-screen`,
-				tw`bg-gradient-to-b from-blue-400 to-green-600`,
+				tw`bg-gradient-to-b from-blue-300 to-green-500`,
 			]}
 		>
 			<main tw="flex flex-col justify-center h-full space-y-5 text-center">
 				<h1 tw="text-white font-black text-6xl">Hello React</h1>
-				<p tw="text-2xl">Developing apps with react is so easy!</p>
+				<p tw="text-3xl">Developing apps with react is so easy!</p>
 			</main>
 		</div>
 	</div>
 );
 
-export { App };
+export {App}
